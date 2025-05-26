74d37601ccf99fdbd9bbb8319e2acc9866e1276d
EmadHouseh <emad.househ1@gmail.com>
2025-05-26 22:05:02 +0300

    start wokrking at home page

--- src/app/pages/main/home/home.component.ts
+++ src/app/pages/main/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '../../../shared/pipes/translate.pipe';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,TranslatePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../core/services/auth/auth.service';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslatePipe } from '../../pipes/translate.pipe';
 
 @Component({
@@ -11,10 +11,19 @@ import { TranslatePipe } from '../../pipes/translate.pipe';
 })
 export class HeaderComponent {
   public auth = inject(AuthService);
+  private router = inject(Router);
   currentLang = 'ar';
+  isHome = false;
 
   ngOnInit(){
     this.currentLang = localStorage.getItem('lang')!;
+    this.isHomePage();
+  }
+
+  isHomePage(): void {
+    this.router.events.subscribe(() => {
+      this.isHome = this.router.url === '/home';
+    })
   }
 
 
