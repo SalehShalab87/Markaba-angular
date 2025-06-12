b35bc1d7d25fee46ed69ee3a986c9e567bec4729
salehAmer <salehshalab2@gmail.com>
2025-06-12 14:01:28 +0300

    Fix Client api end point

--- src/app/shared/constant/constant.ts
+++ src/app/shared/constant/constant.ts
@@ -1,7 +1,9 @@
+import { environment } from "../../../environments/environment.prod";
+
 export const admin = 'admin';
 export const client = 'client';
-export const CarModelsUrl = 'http://localhost:3000/carModels';
-export const CarsUrl = 'http://localhost:3000/cars';
+export const CarModelsUrl = environment.apiUrl+'/carModels';
+export const CarsUrl = environment.apiUrl+'/cars';
 export const cloudName = 'dniaphcwx';
 export const uploadPreset = 'rentCarsMart';
 export const cloudinaryUrl = `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
